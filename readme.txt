e51aecd23e25ed9a085c46f4be509115174db41f
Kyle Douglas <kyle.douglas.0991@gmail.com>
2025-11-03 08:26:58 -0800

    cleanup

--- readme.txt
+++ readme.txt
@@ -1,12 +1,13 @@
 PLAYWRIGHT DEMO
 
-
 Playwright install -> npm init playwright@latest
+
+Add the axe-core library to config: "npm install @axe-core/playwright"
 
 Test headlessly: npx playwrite test
 Test headed:     npx playwrite test --ui
 
-Tests under /tests includes checks for case, length, new password generation,
+Tests under /tests include checks for case, length, new password generation,
 and visual assertions.
 
 There is also a test for accessibility which fails due to existing ADA errors
--- tests/accessibility.spec.ts
+++ tests/accessibility.spec.ts
@@ -1,23 +1,14 @@
-/* TEST: Scan for common accessibility violations.
-NOTE! Test will fail, as page under test has existing ADA violations.
-
-Running this test causes random timeouts in the others when run 
-together. */
-
-
+// TEST: Scan for accessibility issues.
+// NOTE! Page under test has multiple pre-existing violations.
 
 import { test, expect } from '@playwright/test';
 
 // Import the @axe-core/playwright package
-// and install with:   npm install @axe-core/playwright
+// and install with: npm install @axe-core/playwright
 import AxeBuilder from '@axe-core/playwright';
 
-//test.describe('accessibility', () => { 
-  test('expect 0 accessibility violations', async ({ page }) => {
-    await page.goto('https://www.lastpass.com/features/password-generator#generatorTool'); 
-
-    const scanResults = await new AxeBuilder({ page }).analyze();
-
-    expect(scanResults.violations).toEqual([]);
-  });
-//});
+test('accessibility', async ({ page }) => {
+  await page.goto('https://www.lastpass.com/features/password-generator#generatorTool'); 
+  const scanResults = await new AxeBuilder({ page }).analyze();
+  expect(scanResults.violations).toEqual([]);
+});
